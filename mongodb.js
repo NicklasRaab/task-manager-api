eb29761e60f7d5d8c6aff262df53cd3e31da4668
Nicklas Raab <nicklas.raab@gmail.com>
2019-11-21 21:14:22 +0100

    init commit

---
+++ mongodb.js
@@ -0,0 +1,30 @@
+// CRUD operations
+
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    // db.collection('users').deleteMany({
+    //     age: 36
+    // }).then((result) => {
+    //     console.log(result)
+    // }).catch((error) => {
+    //     console.log(error)
+    // })
+
+    db.collection('tasks').deleteOne({
+        description: 'drink beer'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+})
